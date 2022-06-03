75d5a7972a39b987ac6a33a5a2681887e708f967
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-06-04 00:33:59 +0800

    Add pycolumnar gen

--- pycolumnar/include/columnar/ext/pycolumnar.hpp
+++ pycolumnar/include/columnar/ext/pycolumnar.hpp
@@ -21,15 +21,45 @@
 //         PYCOLUMNAR_PYBIND11_PRE, \
 //     ),
 
-#define PYCOLUMNAR_PYBIND11_PRE(Libname, ClsName, ClsDef, ...) \
+#define PYCOLUMNAR_PYBIND11_PRE(Libname, ClsName, ClsDef) \
     PYBIND11_MODULE(Libname, Libname) {
 
-#define PYCOLUMNAR_PYBIND11_POST(Libname, ClsName, ClsDef, ...) \
+#define PYCOLUMNAR_PYBIND11_POST(Libname, ClsName, ClsDef) \
     }
+
+#define PYCOLUMNAR_PYBIND11_CLASS_FIELD(Type, Name, ClsName, ...) \
+    .def_readwrite(#Name, &ClsName::Name)
+
+#define PYCOLUMNAR_PYBIND11_CLASS(Libname, ClsName, ClsDef) \
+    auto py_columnar_class = \
+        pybind11::class_<ClsName>(Libname, #ClsName) \
+            COLUMNAR_STATEMENT_FOREACH2(ClsDef, PYCOLUMNAR_PYBIND11_CLASS_FIELD, ClsName) \
+    ;
+
+#define PYCOLUMNAR_PYBIND11_ARRAY_CLASS(Libname, ClsName, ClsDef) \
+    auto py_columnar_array_class = \
+        pybind11::class_<ClsName::array_t>(py_columnar_class, "array_t") \
+            .def_readonly("size", &ClsName::array_t::size) \
+            .def("__getitem__", &ClsName::array_t::operator[]) \
+    ;
+
+#define PYCOLUMNAR_PYBIND11_HANDLE_CLASS_FIELD(Type, Name, ClsName, ...) \
+    .def(#Name, &ClsName::handle_t::Name, pybind11::return_value_policy::reference)
+
+#define PYCOLUMNAR_PYBIND11_HANDLE_CLASS(Libname, ClsName, ClsDef) \
+    auto py_columnar_handle_class = \
+        pybind11::class_<ClsName::handle_t>(py_columnar_class, "handle_t") \
+            COLUMNAR_STATEMENT_FOREACH2(ClsDef, PYCOLUMNAR_PYBIND11_HANDLE_CLASS_FIELD, ClsName) \
+    ;
+
+#define PYCOLUMNAR_PYBIND11(Libname, ClsName, ClsDef, ...) \
+    PYCOLUMNAR_PYBIND11_PRE(Libname, ClsName, ClsDef) \
+    PYCOLUMNAR_PYBIND11_CLASS(Libname, ClsName, ClsDef) \
+    PYCOLUMNAR_PYBIND11_ARRAY_CLASS(Libname, ClsName, ClsDef) \
+    PYCOLUMNAR_PYBIND11_HANDLE_CLASS(Libname, ClsName, ClsDef) \
+    PYCOLUMNAR_PYBIND11_POST(Libname, ClsName, ClsDef)
 
 #define PYCOLUMNAR_GEN_PYBIND11(...) \
     POST_STRUCT_DEF( \
-        DO(PYCOLUMNAR_PYBIND11_PRE), \
-        /* DO( PYCOLUMNAR_PYBIND11_CLASS ),*/ \
-        DO(PYCOLUMNAR_PYBIND11_POST), \
+        DO(PYCOLUMNAR_PYBIND11), \
     ),
