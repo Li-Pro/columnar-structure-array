f507baa6c13a1f4206bea02d53f578c96bbd674d
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-06-03 22:50:27 +0800

    Allow recursive MAP macro by level

--- pycolumnar/include/columnar/ext/pycolumnar.hpp
+++ pycolumnar/include/columnar/ext/pycolumnar.hpp
@@ -21,15 +21,15 @@
 //         PYCOLUMNAR_PYBIND11_PRE, \
 //     ),
 
-#define PYCOLUMNAR_PYBIND11_PRE(Libname, ClsName, ClsDef) \
+#define PYCOLUMNAR_PYBIND11_PRE(Libname, ClsName, ClsDef, ...) \
     PYBIND11_MODULE(Libname, Libname) {
 
-#define PYCOLUMNAR_PYBIND11_POST(Libname, ClsName, ClsDef) \
+#define PYCOLUMNAR_PYBIND11_POST(Libname, ClsName, ClsDef, ...) \
     }
 
 #define PYCOLUMNAR_GEN_PYBIND11(...) \
     POST_STRUCT_DEF( \
         DO(PYCOLUMNAR_PYBIND11_PRE), \
-        DO( PYCOLUMNAR_PYBIND11_CLASS ), \
-         \
-    )
+        /* DO( PYCOLUMNAR_PYBIND11_CLASS ),*/ \
+        DO(PYCOLUMNAR_PYBIND11_POST), \
+    ),
